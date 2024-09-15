60994d821a266da41f8f23617ad5e4beac9b3eeb
Official Durgesh <durgeshgokhale656@gmail.com>
2024-09-15 10:53:39 +0530

    Auto Fixed

--- config.py
+++ config.py
@@ -1,18 +1,12 @@
 from os import getenv
-from dotenv import_dotenv
-
-load_dotenv()
-
-class Config(object):
-   GER = True
-
-    API_ID = int(getenv("API_ID", 6))
+from dotenv import load_dotenv  # This", 6))
     API_HASH = getenv("API_HASH")
-    DEEP_API = getenvDEEP_API")
-    ARQ_API_KEY = getenv("ARQ_APIKEY", "TBPYLF-SIOYFX-JALTSV-QEAMXE-ARQ")
+    DEEP_API = getenv("DEEP_API")
+    ARQ_API_KEY = getenv("ARQ_API_KEY", "TBPYLF-SIOYFX-JALTSV-QEAMXE-ARQ")
     SPAMWATCH_API = getenv("SPAMWATCH_API", "t9HHtrsmy7faPQWloX8xCvdZK~puDP2RnHLpb~qijQqDj94mhcMQdDP_xO0a_Iwe")
     TOKEN = getenv("TOKEN")
-    OWNER_ID = int(getenv("OWNER_ID", 1679112664))    OWNER_USERNAME = getenv("OWNER_USERNAME", "OfficialDurgesh")
+    OWNER_ID = int(getenv("OWNER_ID", 1679112664))
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "OfficialDurgesh")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "DNS_NETWORK")
     LOGGER_ID = int(getenv("LOGGER_ID", "-1002243074516"))
     MONGO_URI = getenv("MONGO_DB_URI")
@@ -21,14 +15,15 @@ class Config(object):
     DATABASE_URL = getenv("DATABASE_URL")
 
     # Check if DATABASE_URL is set and adjust if needed
-    if DATABASE_URL is not None:
+    if DATABASE_URL:
         if DATABASE_URL.startswith("postgres://"):
- DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql:// 1)
+            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     else:
-        raise ValueError("DATABASE_URL environment variable is not set")
+        print("Warning: DATABASE_URL environment variable is not set.")
+        DATABASE_URL = None  # Or set a default value if appropriate 
 
 class Production(Config):
     LOGGER = True
 
 class Development(Config):
-    LOGGER = True
+    LOGGER = True 
