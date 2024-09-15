f1bd9007c88e46fcfa973a8794d9194214ce35ed
Official Durgesh <durgeshgokhale656@gmail.com>
2024-09-15 11:01:23 +0530

    Auto Fixed

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
+import re
+import sys
 from os import getenv
-from dotenv import load_dotenv  # This", 6))
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+class Config(object):
+    LOGGER = True
+
+    API_ID = int(getenv("API_ID", 6))
     API_HASH = getenv("API_HASH")
     DEEP_API = getenv("DEEP_API")
     ARQ_API_KEY = getenv("ARQ_API_KEY", "TBPYLF-SIOYFX-JALTSV-QEAMXE-ARQ")
     SPAMWATCH_API = getenv("SPAMWATCH_API", "t9HHtrsmy7faPQWloX8xCvdZK~puDP2RnHLpb~qijQqDj94mhcMQdDP_xO0a_Iwe")
     TOKEN = getenv("TOKEN")
     OWNER_ID = int(getenv("OWNER_ID", 1679112664))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "OfficialDurgesh")
+    OWNERenv("OWNER_USERNAME", "OfficialDurgesh")
     SUPPORT_CHAT = getenv("SUPPORT_CHAT", "DNS_NETWORK")
     LOGGER_ID = int(getenv("LOGGER_ID", "-1002243074516"))
     MONGO_URI = getenv("MONGO_DB_URI")
@@ -20,10 +31,10 @@ from dotenv import load_dotenv  # This", 6))
             DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     else:
         print("Warning: DATABASE_URL environment variable is not set.")
-        DATABASE_URL = None  # Or set a default value if appropriate 
+        DATABASE_URL = None  # or set a default value if appropriate
 
 class Production(Config):
     LOGGER = True
 
 class Development(Config):
-    LOGGER = True 
+    LOGGER = True
